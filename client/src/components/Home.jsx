5569ba77b96eed27806c9ac283b46c061fc6a67e
Luna <aleldaproductions@gmail.com>
2023-10-05 22:33:48 -0500

    working on image slider

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,41 @@
+import givemegold1 from '../images/givemegold-2.jpg';
+import givemegold2 from '../images/givemegold-1.jpg';
+import givemegold3 from '../images/givemegold-8.jpg';
+import givemegold4 from '../images/givemegold-6.jpg';
+import givemegold5 from '../images/givemegold-9.jpg';
+import { BsChevronCompactLeft, BsChevronCompactRight} from 'react-icons/bs'
+
 function Home(){
+
+    const image1 = []
+
+    const slides = [
+        {
+            id: 1,
+            url: givemegold1,
+        },
+        {
+            id: 2,
+            url: givemegold2,
+        },
+        {
+            id: 3,
+            url: givemegold3,
+        },
+        {
+            id: 4,
+            url: givemegold4,
+        },
+        {
+            id: 5,
+            url: givemegold5,
+        }
+    ]
+    console.log(givemegold1)
     return(
-        <div>
-            <h1>Hello World</h1>
+        <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative">
+            <div style={{ backgroundImage: `url("${slides[0].url}")`}} className="w-full h-full rounded-2xl bg-center bg-cover duration-500"></div>
+
         </div>
     )
 }
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,30 @@
 import { NavLink, useNavigate } from "react-router-dom"
+import { useState } from "react";
 
 function NavBar() {
+
+
+
     return (
         <div>
-            <nav>
-                <NavLink to="/" className="link1">Home</NavLink>
-                <NavLink to="/about" className="link1">About</NavLink>
-                <NavLink to="/book-us" className="link1">Book Us</NavLink>
-                <NavLink to="/donate" className="link1">Donate</NavLink>
-                <a className="link1" href="https://linktr.ee/givemegoldband">
-                    <img className="insta-link" src="/src/images/linktree2.png" />
-                </a>
+            <nav className="w-full bg-zinc-900 shadow">
+                <ul className="items-center p-5 justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
+                    <li className="font-semibold text-gray-200 hover:text-blue-600">
+                        <NavLink to="/" className="link1">Home</NavLink>
+                    </li>
+                    <li className="font-semibold text-gray-200 hover:text-blue-600">
+                        <NavLink to="/about" className="link1">About</NavLink>
+                    </li>
+                    <li className="font-semibold text-gray-200 hover:text-blue-600">
+                        <NavLink to="/book-us" className="link1">Book Us</NavLink>
+                    </li>
+                    <li className="font-semibold text-gray-200 hover:text-blue-600">
+                        <NavLink to="/donate" className="link1">Donate</NavLink>
+                    </li>
+                    <a className="link1" href="https://linktr.ee/givemegoldband">
+                        <img className="insta-link h-6" src="/src/images/linktree2.png" />
+                    </a>
+                </ul>
             </nav>
         </div>
     )
